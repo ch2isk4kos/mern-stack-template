20c69cbdad36d60cdbe94eb7bea11c08b1cec115
ch2isk4kos <ch2isk4kos@gmail.com>
2022-05-13 19:32:24 -0400

    add environment variables

--- server/index.js
+++ server/index.js
@@ -1,1 +1,8 @@
-console.log("Hello, World!");
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
