080075128b38a2fab3f134e247ec431eb3189b98
ch2isk4kos <ch2isk4kos@gmail.com>
2022-05-15 16:12:38 -0400

    add login functionality to user services

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -43,8 +43,20 @@ const createUser = asyncHandler(async (req, res) => {
 });
 
 const loginUser = asyncHandler(async (req, res) => {
-  //
-  res.json({ message: "Login User" });
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  // compare user with hashed password and respond
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(201).json({
+      _id: user.id,
+      username: user.username,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid credentials");
+  }
 });
 
 const logoutUser = asyncHandler(async (req, res) => {
