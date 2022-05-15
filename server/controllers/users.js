fa24074ef88e559a21d08f54621fefecd5755991
ch2isk4kos <ch2isk4kos@gmail.com>
2022-05-15 16:04:05 -0400

    add functionality to register a user

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,9 +1,45 @@
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs"); // encryption
+const jwt = require("jsonwebtoken"); // web sessions
 const asyncHandler = require("express-async-handler");
+const User = require("../models/User");
 
 const createUser = asyncHandler(async (req, res) => {
-  //
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    res.status(400);
+    throw new Error("Please fill in all fields.");
+  }
+
+  // check if user exists
+  const doesExist = await User.findOne({ email });
+  if (doesExist) {
+    res.status(400);
+    throw new Error("Email is already in use.");
+  }
+
+  // hash user password
+  const salt = await bcrypt.genSalt(10);
+  const hashPassword = await bcrypt.hash(password, salt);
+
+  // create user
+  const user = await User.create({
+    username: username,
+    email: email,
+    password: hashPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      username: user.username,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Error creating user");
+  }
+
   res.json({ message: "Register User" });
 });
 
