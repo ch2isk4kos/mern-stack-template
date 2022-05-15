c426df17534d78161d4db3f642e9ab90c8581745
ch2isk4kos <ch2isk4kos@gmail.com>
2022-05-15 18:25:26 -0400

    BUG FIX: restructure and clean code

--- server/routes/users.js
+++ server/routes/users.js
@@ -14,7 +14,7 @@ const { validateUser } = require("../middlware/auth.js");
 router.post("/", createUser);
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
-router.get("/:id", validateUser, getUser);
+router.get("/", validateUser, getUser);
 router.put("/:id", updateUser);
 router.delete("/:id", deleteUser);
 
