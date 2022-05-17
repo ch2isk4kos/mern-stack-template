3968d4c67ab6a6d628db0db40e0e4f0500f56e6f
ch2isk4kos <ch2isk4kos@gmail.com>
2022-05-16 22:16:44 -0400

    create spinner component to render on load

---
+++ client/src/components/Spinner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Spinner = () => {
+  return (
+    <div id="Spinner" className="loadingSpinnerContainer">
+      <div className="loadingSpinner"></div>
+    </div>
+  );
+};
+
+export default Spinner;
