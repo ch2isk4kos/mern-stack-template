bf5652b320b7190ad002790671487bab66780f70
ch2isk4kos <ch2isk4kos@gmail.com>
2022-05-17 15:44:43 -0400

    map through goals from server and render to dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,15 +1,28 @@
 import React, { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import GoalForm from "../components/GoalForm";
+import Spinner from "../components/Spinner";
+import { getGoals, reset } from "../features/goals/goalSlice";
 
 const Dashboard = () => {
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const { user } = useSelector((state) => state.auth);
+  const { goals, isLoading, isError, message } = useSelector(
+    (state) => state.goals
+  );
 
   useEffect(() => {
+    if (isError) console.log(message);
     if (!user) navigate("/login");
-  }, [user, navigate]);
+    dispatch(getGoals());
+    return () => {
+      dispatch(reset());
+    };
+  }, [user, navigate, isError, message, dispatch]);
+
+  if (isLoading) return <Spinner />;
 
   return (
     <div id="Dashboard">
@@ -18,6 +31,20 @@ const Dashboard = () => {
         <p>Goals Dashboard</p>
       </section>
       <GoalForm />
+      <section className="content">
+        <div
+          className="goals"
+          style={{ display: "flex", flexDirection: "column" }}
+        >
+          {goals && goals.length > 0 ? (
+            goals.map((item) => {
+              return <p key={item._id}>{item.goal}</p>;
+            })
+          ) : (
+            <p>No current goals</p>
+          )}
+        </div>
+      </section>
     </div>
   );
 };
