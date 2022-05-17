9c8e7926555692e53c9531bbc56ba0a07e4002b2
ch2isk4kos <ch2isk4kos@gmail.com>
2022-05-16 22:31:41 -0400

    add conditional render to show logout icon if user is logged in otherwise render login and register icons

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,25 +1,39 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { logout, reset } from "../features/auth/authSlice";
 import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
   return (
-    //
     <header className="header">
       <div className="logo">
         <Link to="/">GoalSetter</Link>
       </div>
       <ul>
-        <li>
-          <Link to="/login">
-            <FaSignInAlt /> Login
-          </Link>
-        </li>
-        <li>
-          <Link to="/register">
-            <FaUser /> Register
-          </Link>
-        </li>
+        {user ? (
+          <li>
+            <Link to="/logout">
+              <FaSignOutAlt /> Logout {user.username}
+            </Link>
+          </li>
+        ) : (
+          <>
+            <li>
+              <Link to="/login">
+                <FaSignInAlt /> Login
+              </Link>
+            </li>
+            <li>
+              <Link to="/register">
+                <FaUser /> Register
+              </Link>
+            </li>
+          </>
+        )}
       </ul>
     </header>
   );
