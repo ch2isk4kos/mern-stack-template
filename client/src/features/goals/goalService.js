23bcb654d942a2cf4eb568ccf1670b2a99eb7ced
ch2isk4kos <ch2isk4kos@gmail.com>
2022-05-17 15:54:28 -0400

    add reducer(s) and action(s) to get all user goals

--- client/src/features/goals/goalService.js
+++ client/src/features/goals/goalService.js
@@ -1,6 +1,17 @@
 import axios from "axios";
 const API_URL = "/api/v1/goals";
 
+const getGoals = async (token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const response = await axios.get(`${API_URL}`, config);
+  return response.data;
+};
+
 const createGoal = async (goal, token) => {
   const config = {
     headers: {
@@ -8,9 +19,9 @@ const createGoal = async (goal, token) => {
     },
   };
 
-  const response = await axios.post(`${API_URL}/`, goal, config);
+  const response = await axios.post(`${API_URL}`, goal, config);
   return response.data;
 };
 
-const goalService = { createGoal };
+const goalService = { getGoals, createGoal };
 export default goalService;
--- client/src/features/goals/goalSlice.js
+++ client/src/features/goals/goalSlice.js
@@ -9,12 +9,30 @@ const initialState = {
   message: "",
 };
 
+// create new goal
 export const createGoal = createAsyncThunk(
   "goals/create",
   async (goal, thunkAPI) => {
     try {
       const token = thunkAPI.getState().auth.user.token;
       return await goalService.createGoal(goal, token);
+    } catch (error) {
+      const message =
+        (error.response && error.response.data && error.response.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
+// get user goals
+export const getGoals = createAsyncThunk(
+  "goals/getAll",
+  async (_, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token;
+      return await goalService.getGoals(token);
     } catch (error) {
       const message =
         (error.response && error.response.data && error.response.message) ||
@@ -43,6 +61,19 @@ export const goalSlice = createSlice({
         state.goals.push(action.payload);
       })
       .addCase(createGoal.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      })
+      .addCase(getGoals.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getGoals.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.goals = action.payload;
+      })
+      .addCase(getGoals.rejected, (state, action) => {
         state.isLoading = false;
         state.isError = true;
         state.message = action.payload;
